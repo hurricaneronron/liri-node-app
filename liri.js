5582d389aca7dd615e83519f42fcbb4d8019b04b
hurricaneronron <vprzybyl@gmail.com>
2018-03-27 09:41:09 -0700

    add spotify API

--- liri.js
+++ liri.js
@@ -2,6 +2,15 @@ require('dotenv').config( )
 
 var keys = require('./keys.js')
 
-var spotify = new Spotify(keys.spotify)
-var client = new Twitter(keys.twitter)
+var twitter = keys.twitter
+var spotify = keys.spotify
 
+// console.log(process.env.TWITTER_CONSUMER_KEY)
+
+// test connection to keys.js
+console.log(twitter.consumer_key)
+console.log(twitter.consumer_secret)
+console.log(twitter.access_token_key)
+console.log(twitter.access_token_secret)
+console.log(spotify.id)
+console.log(spotify.secret)
